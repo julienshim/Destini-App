923f94251f594db3c50974aaedae7e9c343beaac
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-12-05 19:45:19 -0800

    first commit

---
+++ Destini/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Julien Shim on 12/5/18.
+//  Copyright © 2018 Julien Shim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
