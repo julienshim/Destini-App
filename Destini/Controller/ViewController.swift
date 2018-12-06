3b4b5d2e3ebf518be577fb95a1704a3ec0bef1d4
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-12-06 13:30:13 -0800

    loaded text on load

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let allStories = StoryBank()
+    var storyIndex = 0;
 
+    @IBOutlet weak var storyTextView: UILabel!
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        loadText()
+        
     }
-
-
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        print(sender.tag)
+        
+        
+    }
+    
+    func loadText () {
+        
+        storyTextView.text = allStories.list[storyIndex].storyText
+        
+        // Annoying Attributed Styling
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.white,
+            .paragraphStyle: paragraphStyle
+            
+        ]
+        
+        topButton.setAttributedTitle(NSAttributedString(string: allStories.list[storyIndex].choiceA, attributes: attributes), for: .normal)
+        bottomButton.setAttributedTitle(NSAttributedString(string: allStories.list[storyIndex].choiceB, attributes: attributes), for: .normal)
+        
+    }
+    
+    
 }
 
