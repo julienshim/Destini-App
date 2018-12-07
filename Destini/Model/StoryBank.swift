00c816e689da6ee744e8effc387bf6848d8bd298
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-12-07 10:19:18 -0800

    removed pauses, corrected some typos

--- Destini/Model/StoryBank.swift
+++ Destini/Model/StoryBank.swift
@@ -17,22 +17,22 @@ class StoryBank {
     init() {
         
         // Story 1
-        list.append(Story(text: "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception.\(pause) You decide to hitchhike.\(pause) A rusty pickup truck rumbles to a stop next to you.\(pause) A man with a wide brimmed hat and souless eyes opens the passenger door for you$ and you says, \"Need a ride, boy?\"", a: "I'll hope in. Thanks for the help!", b: "Well, I don't have many options. Better ask him if he's a murderer.", p: 3))
+        list.append(Story(text: "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception.\(pause) You decide to hitchhike.\(pause) A rusty pickup truck rumbles to a stop next to you.\(pause) A man with a wide brimmed hat and souless eyes opens the passenger door for you$ and you says, \"Need a ride, boy?\"", a: "I'll hop in. Thanks for the help!", b: "Well, I don't have many options. Better ask him if he's a murderer."))
         
         // Story 2
-        list.append(Story(text: "He nods slowly, unphased by the question.", a: "At least he's honest. I'll climb in.", b: "Wait, I know how to change a tire.", p: 3))
+        list.append(Story(text: "He nods slowly, unphased by the question.", a: "At least he's honest. I'll climb in.", b: "Wait, I know how to change a tire."))
         
         // Story 3
-        list.append(Story(text: "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glove box. Inside you find a bloody knife, two severed fingers, and a casette tape of Elton John. He reaches for the glove box.", a: "I love Elton John! Hand him the cassette tape.", b: "It's hime or me. Take the knife and stab him.", p: 3))
+        list.append(Story(text: "As you begin to drive, the stranger starts talking about his relationship with his mother.\(pause) He gets angrier and angrier by the minute.\(pause) He asks you to open the glove box.\(pause) Inside you find a bloody knife, two severed fingers, and a casette tape of Elton John.\(pause) He reaches for the glove box.", a: "I love Elton John! Hand him the cassette tape.", b: "It's him or me. Take the knife and stab him."))
         
         // Story 4
-        list.append(Story(text: "What? Such a copy out! Did you know accidental traffic accidents are the second leading cause of accidental death for most adult age groups?", a: "", b: "", p: 3))
+        list.append(Story(text: "What?\(pause) Such a copy out!\(pause) Did you know accidental traffic accidents are the second leading cause of accidental death for most adult age groups?", a: "", b: ""))
         
         // Story 5
-        list.append(Story(text: "As you smash through the guardrail and careen toward the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.", a: "", b: "", p: 3))
+        list.append(Story(text: "As you smash through the guardrail and careen toward the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.", a: "", b: ""))
         
         // Story 6
-        list.append(Story(text: "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks if you know any good places to dump bodies. You reply: \"Try the pier.\"", a: "", b: "", p: 3))
+        list.append(Story(text: "You bond with the murderer while crooning verses of \"Can you feel the love tonight\".\(pause) He drops you off at the next town.\(pause) Before you go he asks if you know any good places to dump bodies.\(pause) You reply: \"Try the pier.\"", a: "", b: ""))
         
     }
 
